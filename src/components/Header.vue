a0b6b959a5883f51c2520a4305fd23911c8caf3e
Martin Matousek <martin.matousek@outlook.com>
2023-08-20 11:44:34 +0200

    Responsive navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,11 +25,11 @@ export default {
 <template>
   <Disclosure as="nav" class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
+      <div class="relative flex h-16 items-center justify-between z-10">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton @click="this.open = !this.open"
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white z-10">
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!this.open" class="block h-6 w-6" aria-hidden="true" />
@@ -55,7 +55,7 @@ export default {
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
         <router-link v-for="item in navigation" :key="item.name" :to="item.href"
-          :class="[item.name == $route.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium z-20']"
+          :class="[item.name == $route.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium relative z-10']"
           :aria-current="item.name == $route.name ? 'page' : undefined">{{ item.name }}</router-link>
       </div>
     </DisclosurePanel>
