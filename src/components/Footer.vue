28a6c1ea53428617b9e6a7e81231e49a3a378b2a
Martin Matousek <martin.matousek@outlook.com>
2023-08-22 16:20:13 +0200

    Back to the roots

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@
                 </a>
             </div>
             <div class="mt-8 md:order-1 md:mt-0">
-                <p class="text-center text-xs leading-5 text-gray-500">&copy; 2023 Tame Rift. All rights reserved.
+                <p class="text-center text-xs leading-5 text-gray-500">&copy; 2023 Tamed Rift. All rights reserved.
                 </p>
             </div>
         </div>
